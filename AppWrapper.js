3fe69787d11ae8fde95062bde48220134a4e531f
Shashank Acharya <acharya.s@icloud.com>
2022-07-31 19:40:11 +0800

    animation fixes

--- AppWrapper.js
+++ AppWrapper.js
@@ -13,8 +13,8 @@ const Stack = createNativeStackNavigator()
 export default function AppWrapper() {
 	return (
 		<Stack.Navigator initialRouteName={'Authentication'}>
-			<Stack.Screen name="Authentication" component={AuthScreen} options={{ headerShown: false, animation: 'none' }}/>
-			<Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, animation: 'none' }}/>
+			<Stack.Screen name="Authentication" component={AuthScreen} options={{ headerShown: false }}/>
+			<Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
 			<Stack.Screen name="Scan" component={ScanScreen} options={{ headerShown: false }}/>
 			<Stack.Screen name="Merchants" component={MerchantsScreen} options={{ headerShown: false }}/>
 			<Stack.Screen name="Merchant" component={MerchantScreen} options={{ headerShown: false }}/>
