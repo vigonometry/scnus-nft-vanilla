c0ccb6cbbce874736e3ae90128b50b954b72d561
Shashank Acharya <acharya.s@icloud.com>
2022-08-02 12:22:11 +0800

    fixed redirect bug on android

--- misc/wc-tools/providers/WalletConnectProvider.tsx
+++ misc/wc-tools/providers/WalletConnectProvider.tsx
@@ -64,7 +64,7 @@ export default function WalletConnectProvider({
         const canOpenURL = await Linking.canOpenURL(uri);
         if (!canOpenURL) {
           // Redirect the user to download a wallet.
-          Linking.openURL('https://walletconnect.org/wallets');
+          Linking.openURL('https://walletconnect.com/registry?type=wallet');
           throw new Error('No wallets found.');
         }
         await Linking.openURL(uri);
