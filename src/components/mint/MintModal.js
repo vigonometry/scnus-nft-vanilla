56642b547fc4052f92d240178d18037ddebf64eb
Shashank Acharya <acharya.s@icloud.com>
2022-08-02 12:22:01 +0800

    added mint success/failure modal

--- src/components/mint/MintModal.js
+++ src/components/mint/MintModal.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 import { useWeb3ExecuteFunction } from "react-moralis";
 import { Image, Modal, Text, TouchableOpacity, View } from "react-native";
 import close from '../../../assets/icons/x.png'
@@ -6,7 +7,10 @@ import { DAPP_ABI, DAPP_ADDRESS } from "../../../contractConfig";
 import useScaNUSTokens from "../../../misc/hooks/useScaNUSTokens";
 
 export default function MintModal(props) {
-	const { fetch } = useWeb3ExecuteFunction()
+	const [isSuccess, setIsSuccess] = useState(undefined)
+	const navigation = useNavigation()
+	// TRY useApiContract() hook from hooks folder to minimise
+	const { fetch, error, data } = useWeb3ExecuteFunction()
 	const makeOptions = (tokenId) => ({
 		abi: DAPP_ABI,
 		contractAddress: DAPP_ADDRESS,
@@ -19,14 +23,20 @@ export default function MintModal(props) {
 	const mint = () => {
 		if (props.token) {
 			fetch({ params: makeOptions(props.token.tokenId)})
-				.then(res => {
-					addToken(props.token.tokenId)
-					props.close()
-				})
-				.catch(e => {
-					console.log(e)
-				})
 		}
+	}
+	useEffect(() => {
+		if (data) {
+			addToken(props.token.tokenId)
+			setIsSuccess(true)
+		}
+		else if (error) setIsSuccess(false)
+		else setIsSuccess(undefined)
+	}, [data, error])
+	const backHome = () => {
+		setIsSuccess(undefined)
+		props.close()
+		navigation.goBack()
 	}
 	return (
 		<Modal visible={!!props.token} transparent={true} onRequestClose={props.close}>
@@ -35,15 +45,21 @@ export default function MintModal(props) {
 					<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
 						<View style={{ width: '90%', backgroundColor: 'white', borderRadius: 10 }}>
 							<View style={{ display: 'flex', padding: 30, paddingBottom: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-								<Text style={{ fontSize: 20, fontWeight: 'bold' }}>Mint Token</Text>
-								<TouchableOpacity onPress={props.close}>
+								<Text style={{ fontSize: 20, fontWeight: 'bold' }}>{ isSuccess === undefined ? 'Mint Token' : isSuccess ? 'Success!' : 'Failed :('}</Text>
+								<TouchableOpacity onPress={() => { setIsSuccess(undefined); props.close(); }}>
 									<Image source={close} style={{ width: 30, height: 30 }}/>
 								</TouchableOpacity>
 							</View>
 							<View style={{ padding: 30, paddingTop: 10 }}>
-								<Text style={{ fontSize: 18, lineHeight: 18*1.618 }}>You found a new token! You are now able to mint <Text style={{ fontWeight: 'bold' }}>{props.token.name}</Text></Text>
-								<TouchableOpacity onPress={mint} activeOpacity={0.8} style={{ backgroundColor: '#364378', padding: 12, marginTop: 24, alignItems: 'center', width: 120, borderRadius: 12 }}>
-									<Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white' }}>Mint!</Text>
+								{isSuccess === undefined ? (
+									<Text style={{ fontSize: 18, lineHeight: 18*1.618 }}>You found a new token! You are now able to mint <Text style={{ fontWeight: 'bold' }}>{props.token.name}</Text></Text>
+								) : isSuccess ? (
+									<Text style={{ fontSize: 18, lineHeight: 18*1.618 }}>You have successfully minted <Text style={{ fontWeight: 'bold' }}>{props.token.name}</Text></Text>
+								) : (
+									<Text style={{ fontSize: 18, lineHeight: 18*1.618 }}>There was an error while minting <Text style={{ fontWeight: 'bold' }}>{props.token.name}</Text></Text>
+								)}
+								<TouchableOpacity onPress={isSuccess ? backHome : mint} activeOpacity={0.8} style={{ backgroundColor: isSuccess === undefined ? '#364378' : isSuccess ? '#27ae60' : '#c0392b', padding: 12, marginTop: 24, alignItems: 'center', width: 160, borderRadius: 12 }}>
+									<Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white' }}>{ isSuccess === undefined ? 'Mint!' : isSuccess ? 'Back Home' : 'Try Again' }</Text>
 								</TouchableOpacity>
 							</View>
 						</View>
